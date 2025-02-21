260089095348df13b80a30fae928b0cbacf0c9eb
Wan3d <zullojeanpieronicolas@gmail.com>
2025-02-20 23:40:48 -0600

    Disappearing buttons

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from discord.ui import Button, View
 import random
 import asyncio
 
-
 class MyClient(commands.Bot):
 
     async def on_ready(self):
@@ -21,9 +20,6 @@ class MyClient(commands.Bot):
             return
         if message.content.startswith('I need help'):
             await message.channel.send(f'What do you need, {message.author}?')
-        elif message.content.startswith('!guesserInfo'):
-            attempts = 15
-            await message.channel.send(f"A random number will be generated between the range of 1 and 1000. You have {attempts} attempts to guess it.\nThink fast, because you have only 15 seconds to type each answer.\n'!guesserCommands' to see how many commands you can use.")
     async def on_reaction_add(self, reaction, user):
         await reaction.message.channel.send('Do not react')
 intents = discord.Intents.default()
@@ -35,7 +31,6 @@ GUILD_ID = discord.Object(id=1090322790051229806)
 @client.tree.command(name="hello", description="Just hello", guild=GUILD_ID)
 async def sayHello(interaction: discord.Interaction):
     await interaction.response.send_message("Hello!")
-
 @client.tree.command(name="printer", description="I will print whatever you get me", guild=GUILD_ID)
 async def printer(interaction: discord.Interaction, printer: str):
     await interaction.response.send_message(printer)
@@ -49,63 +44,43 @@ async def printEmbed(interaction: discord.Interaction):
     embed.set_author(name=interaction.user.name, url="https://www.instagram.com/zullojeanpiero/")
     await interaction.response.send_message(embed=embed)
 class View(discord.ui.View):
-    @discord.ui.button(label="Start", style=discord.ButtonStyle.blurple)
+    @discord.ui.button(label="Start", style=discord.ButtonStyle.green)
     async def button_callback(self, interaction, button):
+        button.disabled = True
+        await interaction.response.edit_message(view=self) #Edits the message to make disappear the button 
         number = random.randint(1,1000)
-        await interaction.response.send_message(":white_check_mark: Random number generated :white_check_mark:")
+        await interaction.followup.send(":white_check_mark: Random number generated :white_check_mark:")
         def check(m):
             return m.author == interaction.user and m.channel == interaction.channel
         attempts = 15
+        cont = 0
         for i in range(attempts):
             try:
                 msg = await interaction.client.wait_for("message", timeout=15.0, check=check)
                 guess = int(msg.content)
+                cont += 1
                 if (number == guess):
-                    await interaction.followup.send(f"YOU HAVE GUESSED THE NUMBER :fire: CONGRATSSSS! :partying_face:")
+                    await interaction.followup.send(f"YOU HAVE GUESSED THE NUMBER IN {cont} ATTEMPTS :fire: :fire: CONGRATSSSS! :partying_face:")
                     return
                 elif (number > guess):
-                    await interaction.followup.send("Too low!")
+                    await interaction.followup.send("Too low! :arrow_down:")
                 elif (number < guess):
-                    await interaction.followup.send("Too high!")
+                    await interaction.followup.send("Too high! :arrow_up: ")
             except asyncio.TimeoutError:
                 await interaction.followup.send(":clock: You took to long to guess. Game ended.")
                 return
-        await interaction.followup.send(f"You have reached the attempts limit. The number was {number}.")
-    @discord.ui.button(label="Info", style=discord.ButtonStyle.green)
-    async def second_button_callback(self, button, interaction):
+        await interaction.followup.send(f"You have reached the attempts limit :x: The number was {number} :sob:")
+    @discord.ui.button(label="Info", style=discord.ButtonStyle.blurple)
+    async def second_button_callback(self, interaction, button):
+        button.disabled = True
+        await interaction.response.edit_message(view=self) #Edits the message to make disappear the button 
         attempts = 15
-        await button.response.send_message(f"A random number will be generated between the range of 1 and 1000. You have {attempts} attempts to guess it.\nThink fast, because you have only 15 seconds to type each answer. ")
-    @discord.ui.button(label="Commands", style=discord.ButtonStyle.gray)
-    async def third_button_callback(self, button, interaction):
-        await button.response.send_message("Here are the list of commands: \n")
+        await interaction.followup.send(f":white_circle: A random number will be generated between the range of 1 and 1000. You have {attempts} attempts to guess it.\n:warning: Think fast, because you have only 15 seconds to type each answer.")
 @client.tree.command(name="guess_the_number", description="Ready to play?", guild=GUILD_ID)
-async def guessingButton(interaction: discord.Interaction):
-    await interaction.response.send_message("Before starting, if you don't know how to play you should press the 'Info' button and the 'Commands' button if you want to know some commands.\nWhenever you are ready, press the 'Start' button.", view=View())
+async def guessingButton(interaction):
+    await interaction.response.send_message("Before starting, if you don't know how to play you should press the 'Info' button.\nWhenever you are ready, press the 'Start' button.", view=View())
 from config import TOKEN
 client.run(TOKEN)
 
-'''@client.tree.command(name="guessing", description="Guess the random number", guild=GUILD_ID)
-async def genNumber(interaction: discord.Interaction):
-    await interaction.response.send_message("Before starting, if you don't know how to play you should write '!guesserInfo'")
-    number = random.randint(1,1000)
-    await interaction.followup.send(":white_check_mark: Random number generated :white_check_mark:")
-    attempts = 15
-    def check(m):
-        return m.author == interaction.user and m.channel == interaction.channel
-    for i in range(attempts):
-        try:
-            msg = await client.wait_for("message", timeout=15.0, check=check)
-            guess = int(msg.content)
-            if guess == number:
-                await interaction.followup.send(f"YOU HAVE GUESSED THE NUMBER :fire: CONGRATSSSS! :partying_face:")
-                return
-            elif guess > number:
-                await interaction.followup.send("Too high!")
-            elif guess < number:
-                await interaction.followup.send("Too low!")
-        except asyncio.TimeoutError:
-            await interaction.followup.send(":clock: You took to long to guess. Game ended.")
-            return
-    await interaction.followup.send(f"You have reached the attempts limit. The number was {number}.")'''
 
 
